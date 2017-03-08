91eaad18f22011d699a4bb0c091d59d9fa7ae9eb
Dervis <dervis85@gmail.com>
2017-03-08 10:10:30 +0100

    First commit

---
+++ Labb1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Labb1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("G test begins" + "\n");
+			Console.WriteLine(new Position(2, 4) + new Position(1, 2) + "\n");
+			Console.WriteLine(new Position(2, 4) - new Position(1, 2) + "\n");
+			Console.WriteLine(new Position(1, 2) - new Position(3, 6) + "\n");
+			Console.WriteLine(new Position(3, 5) % new Position(1, 3) + "\n");
+
+			SortedPosList list1 = new SortedPosList();
+			SortedPosList list2 = new SortedPosList();
+			list1.Add(new Position(3, 7));
+			list1.Add(new Position(1, 4));
+			list1.Add(new Position(2, 6));
+			list1.Add(new Position(2, 3));
+
+			Console.WriteLine(list1 + "\n");
+			list1.Remove(new Position(2, 6));
+			Console.WriteLine(list1 + "\n");
+
+			list2.Add(new Position(3, 7));
+			list2.Add(new Position(1, 2));
+			list2.Add(new Position(3, 6));
+			list2.Add(new Position(2, 3));
+
+			Console.WriteLine((list2 + list1) + "\n");
+
+			SortedPosList circleList = new SortedPosList();
+			circleList.Add(new Position(1, 1));
+			circleList.Add(new Position(2, 2));
+			circleList.Add(new Position(3, 3));
+			Console.WriteLine(circleList.CircleContent(new Position(5, 5), 4) + "\n");
+			Console.WriteLine("G Test end" + "\n");
+			Console.WriteLine("VG test begins" + "\n");
+
+			Console.WriteLine("Tests for subtraction operator. Removes positions in list 1 that exists in list2 " +
+							  "and prints numbers in list1. Should return (1,4)");
+
+			Console.WriteLine("End of comment" + "\n");
+			Console.WriteLine(list1 - list2 + "\n");
+
+			Console.WriteLine("Tests for subtraction operator. Removes positions in list2 that exists in list1 " +
+							  "and prints numbers in list1. Should return (1,2) and (3,6)");
+
+			Console.WriteLine("End of comment" + "\n");
+			Console.WriteLine(list2 - list1 + "\n");
+
+			Console.WriteLine("Test for adding positions to a synced list. Creates a new SortedPosList with filePath " +
+							  "argument. Adds positions to the list and removes one position and then Loads the file " +
+							  "and displays the file content in console. Should display (8, 2)(8,3)(8,7) first time" +
+			                  "and 6 second time and so on");
+			Console.WriteLine("End of comment" + "\n");
+
+
+			SortedPosList list3 = new SortedPosList("test.txt");
+
+			list3.Add(new Position(8, 7));
+			list3.Add(new Position(8, 2));
+			list3.Add(new Position(8, 6));
+			list3.Add(new Position(8, 3));
+			list3.Remove(new Position(8, 6));
+			Console.WriteLine(list3);
+			Console.WriteLine("\n");
+
+			Console.WriteLine("Test for reading from file. Creates a new SortedPosList with filePath argument. Loads the file and " +
+				"displays a list of positions. The file should display a list of positions written by the user.");
+			Console.WriteLine("End of comment" + "\n");
+
+			//You need a text2.txt filled with positions for this to work or it will not show anything.
+			SortedPosList list4 = new SortedPosList("test2.txt");
+			Console.WriteLine(list4);
+			Console.WriteLine("\n");
+
+			Console.WriteLine("VG test end" + "\n");
+
+			list3.Add(new Position(5, 5));
+			list3.Add(new Position(4, 5));
+			list3.Add(new Position(33, 6));
+			list3.Add(new Position(11, 3));
+
+		}
+	}
+}
---
+++ Labb1/Properties/SortedPosList.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace Labb1
+{
+	public class SortedPosList
+	{
+
+		private List<Position> posList = new List<Position>();
+
+		private string SavedFilePath { get; set; }
+
+		public Position this[int i]
+		{
+			get { return posList[i]; }
+		}
+
+		public int Count()
+		{
+			return posList.Count;
+		}
+		public void Add(Position pos)
+		{
+			for (int i = 0; i < Count(); i++)
+			{
+				if (pos < posList[i])
+				{
+					posList.Insert(i, pos);
+					if (SavedFilePath != null)
+					{
+						Save();
+					}
+					return;
+				}
+			}
+			posList.Add(pos);
+		}
+		public bool Remove(Position pos)
+		{
+			bool removed = false;
+			for (int i = 0; i < Count(); i++)
+			{
+				if (pos.Equals(posList[i]))
+				{
+					posList.RemoveAt(i);
+					i--;
+					removed = true;
+					if (SavedFilePath != null)
+					{
+						Save();
+					}
+				}
+			}
+            return removed;
+		}
+		public SortedPosList Clone()
+		{
+			SortedPosList newSortedPosList = new SortedPosList();
+			foreach (Position p in posList)
+			{
+				newSortedPosList.Add(p.Clone());
+			}
+			return newSortedPosList;
+        }
+
+		public SortedPosList CircleContent(Position centerPos, double radius)
+		{
+			SortedPosList newSortedPList = new SortedPosList();
+			double dis;
+			foreach (Position p in posList)
+			{
+				dis = Math.Sqrt(Math.Pow(centerPos.XValue - p.XValue, 2) + (Math.Pow(centerPos.YValue - p.YValue, 2)));
+
+				if (dis < radius)
+				{
+					newSortedPList.Add((p.Clone()));
+				}
+			}
+			return newSortedPList;
+		}
+
+		public static SortedPosList operator +(SortedPosList sp1, SortedPosList sp2)
+		{
+			SortedPosList newSortedPList = sp1.Clone();
+
+			for (int i = 0; i < sp2.Count(); i++)
+			{
+				newSortedPList.Add(sp2[i].Clone());
+			}
+            return newSortedPList;
+		}
+
+		public static SortedPosList operator -(SortedPosList sp1, SortedPosList sp2)
+		{
+			SortedPosList newSortedPList = sp1.Clone();
+
+			for (int i = 0; i < sp2.Count(); i++)
+			{
+				newSortedPList.Remove(sp2[i]);
+			}
+			return newSortedPList;
+		}
+
+		public SortedPosList()
+		{
+			posList = new List<Position>();
+			SavedFilePath = null;
+		}
+
+		public SortedPosList(string filePath)
+		{
+			SortedPosList syncedSortedPList = new SortedPosList();
+			syncedSortedPList.SavedFilePath = filePath;
+			SavedFilePath = filePath;
+
+			if (File.Exists(filePath))
+			{
+                File.ReadAllText(SavedFilePath);
+				Load();
+            }
+			else
+			{
+				File.Create(SavedFilePath).Dispose();
+			}
+		}
+
+		public void Save()
+		{
+			File.WriteAllText(SavedFilePath, string.Join("\n", posList));
+
+		}
+		public void Load()
+		{
+			char[] sep1 = { '\n' };
+			char[] sep2 = { ',', ')', '(' };
+			string[] posArr;
+			string file = File.ReadAllText(SavedFilePath);
+			string[] posNr = file.Split(sep1, StringSplitOptions.RemoveEmptyEntries);
+
+			for (int i = 0; i < posNr.Length; i++)
+			{
+				posArr = posNr[i].Split(sep2, StringSplitOptions.RemoveEmptyEntries);
+				posList.Add(new Position(Int32.Parse(posArr[0]), Int32.Parse(posArr[1])));
+            }
+        }
+
+		public override string ToString()
+        {
+            return string.Join(", ", posList);
+        }
+    }
+}
